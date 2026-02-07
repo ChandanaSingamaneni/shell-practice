7d18d90d6da06e25899042971f22d2e4f5e8e88a
Chandana <chandanachowdary399@gmail.com>
2026-02-07 12:20:42 +0530

    Caluclate script run time in sec

--- 06-Timestamp.sh
+++ 06-Timestamp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-Timestamp=$(date)
+START_TIME=$(date +%s)
 
-echo "Script executed time:$Timestamp"
+echo "Script started at: $START_TIME"
+
+sleep 15
+
+END_TIME=$(date +%s)
+echo "Script end at: $END_TIME"
+
+TOTAL_TIME=$(($END_TIME-$START_TIME-))
+echo "Script executed in: $TOTAL_TIME"
