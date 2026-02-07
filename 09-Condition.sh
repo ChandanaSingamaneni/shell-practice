4c6c0c771c0eecf6d676f2a15da3b83302857185
Chandana <chandanachowdary399@gmail.com>
2026-02-07 13:42:56 +0530

    Conditions

--- 09-Condition.sh
+++ 09-Condition.sh
@@ -7,10 +7,10 @@ NUMBER=$1
 #eq -> equal to
 #ne -> not equal to
 
-if [NUMBER -gt 20]; then
+if [$NUMBER -gt 20]; then
     echo "Number is greater than 20"
 
-else if [Number -eq 20]; then
+else if [$Number -eq 20]; then
     echo "Number equal to 20"
 else
     echo "Number is less than 20"
