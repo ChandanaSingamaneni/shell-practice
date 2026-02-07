eba4131fbd64f252346b3ff11ed12eaeeb727cac
Chandana <chandanachowdary399@gmail.com>
2026-02-07 11:33:41 +0530

    Using variables

---
+++ 03.Variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Chandana #No space between value and =
+PERSON2=Chowdary
+echo "$PERSON1: Hello $PERSON2, How are you"
+echo "$PERSON2: Hi $PERSON1, I am fine thanks for asking, How are you doing?"
+echo "$PERSON1: Doing fine. How is your work going"
+echo "$PERSON2: I am learning shell scripting, my team what to start automate the works. What about you?"
