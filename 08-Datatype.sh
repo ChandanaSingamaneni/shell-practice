baa13cacff47ac3a9a03e8a2ec67a08449eeab79
Chandana <chandanachowdary399@gmail.com>
2026-02-07 13:19:08 +0530

    DATATYPES

--- 08-Datatype.sh
+++ 08-Datatype.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-#
+echo "first number is:: "
+read FIRSTNUMBER
+
+echo "Second number is :: "
+read SECONDNUMBER
+
+SUM=$((FIRSTNUMBER + SECONDNUMBER))
+echo "Sum is : $SUM"
 
 #Array
 FRUITS=("Apple" "Banana" "Guva")
 
 echo "Fruits are: ${FRUITS[@]}"
+echo "First fruit is : ${FRUITS[0]}"
+echo "SECOND fruit is : ${FRUITS[1]}"
+echo "THIRD fruit is : ${FRUITS[2]}"
