f64e77dae0271ea590cff58d5889394e8913b160
Chandana <chandanachowdary399@gmail.com>
2026-02-07 11:24:24 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Hello Ramesh, How are you"
+echo "Ramesh: Hi Suresh, I am fine thanks for asking, How are you doing?"
+echo "Suresh: Doing fine. How is your work going"
+echo "Ramesh: I am learning shell scripting, my team what to start automate the works. What about you?"
