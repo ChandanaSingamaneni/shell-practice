ba09f88fc2c974463d2553ba06e33dfc166aa520
Chandana <chandanachowdary399@gmail.com>
2026-02-07 11:41:54 +0530

    Using arguments

---
+++ 04-arugments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 #No space between value and =
+PERSON2=$2
+
+echo "$1: Hello $2, How are you"
+echo "$2: Hi $1, I am fine thanks for asking, How are you doing?"
+echo "$1: Doing fine. How is your work going"
+echo "$2: I am learning shell scripting, my team what to start automate the works. What about you?"
