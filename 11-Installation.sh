2beb941d7bce29744e0bf241f3db5b5eeed371e5
Chandana <chandanachowdary399@gmail.com>
2026-02-08 22:11:27 +0530

    Installation

--- 11-Installation.sh
+++ 11-Installation.sh
@@ -10,10 +10,28 @@ fi
 echo "Installing Nginx"
 dnf install nginx -y
 
-if [ $USER -ne 0]; then
+if [ $USER -ne 0 ]; then
     echo "Installing nginx ... FAILURE"
     exit 1
 else 
     echo "Installing nginx ... SUCCESS"
+fi
+
+dnf install mysql -y
+
+if [ $USER -ne 0 ]; then
+    echo "Installing mysql ... FAILURE"
+    exit 1
+else 
+    echo "Installing mysql ... SUCCESS"
+fi
+
+dnf install nodejsi -y
+
+if [ $USER -ne 0 ]; then
+    echo "Installing nodejs ... FAILURE"
+    exit 1
+else 
+    echo "Installing nodejs ... SUCCESS"
 fi
    
